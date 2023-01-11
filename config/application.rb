516466f3fdae6c1e1c90e6db28287f1e91c0568f
Samuelfaure <samuel.faure.dev@gmail.com>
2023-01-11 06:33:58 +0100

    fix startup bug

--- config/application.rb
+++ config/application.rb
@@ -15,15 +15,17 @@ Bundler.require(*Rails.groups)
 #
 # config.time_zone = "Central Time (US & Canada)"
 # config.eager_load_paths << Rails.root.join("extras")
-class Reprogrammer::Application < Rails::Application
-  # Initialize configuration defaults for originally generated Rails version.
-  config.load_defaults 7.0
+module Reprogrammer
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.0
 
-  # Configuration for the application, engines, and railties goes here.
-  #
-  # These settings can be overridden in specific environments using the files
-  # in config/environments, which are processed later.
-  #
-  # config.time_zone = "Central Time (US & Canada)"
-  # config.eager_load_paths << Rails.root.join("extras")
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+  end
 end
